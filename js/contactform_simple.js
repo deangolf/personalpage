305d5b4a1f37623cb377ac2b27f62045571159b4
deangolf <50503597+deangolf@users.noreply.github.com>
2020-06-06 23:18:30 +0200

    Contact form (dopuna) - dodana je provjera valjanosti svih polja prije submita.

--- js/contactform_simple.js
+++ js/contactform_simple.js
@@ -10,31 +10,43 @@ $(document).ready(function () {
         var message = $(document.getElementById("question")).val()
         var statusElm = $('.statusElm')
         statusElm.empty()
+        var i = 0
 
         if(fullName.length>5 ) {
             statusElm.append('<div>Full Name is valid<div/>')
+            i = i+1
         } else {
             event.preventDefault()
             statusElm.append('<div>Full Name is not valid<div/>')
         }
         if(email.length>5 && email.includes('@') && email.includes('.')) {
             statusElm.append('<div>Email is valid<div/>')
+            i = i+1
         } else {
             event.preventDefault()
             statusElm.append('<div>Email is not valid<div/>')
         }
         if(phone.length>5 ) {
             statusElm.append('<div>Phone is valid<div/>')
+            i = i+1
         } else {
             event.preventDefault()
             statusElm.append('<div>Phone is not valid<div/>')
         }
         if(message.length>20 ) {
             statusElm.append('<div>Message is valid<div/>')
+            i = i+1
         } else {
             event.preventDefault()
             statusElm.append('<div>Message is not valid<div/>')
         }
-        document.getElementById("contactForm").submit();
+        if(i === 4) {
+            document.getElementById("contactForm").submit();
+        } else {
+            statusElm.append('<div style="color:red">Please check entries, something appears missing.<div/>')
+        }
         })
 })
+function submitForm() {
+
+}
